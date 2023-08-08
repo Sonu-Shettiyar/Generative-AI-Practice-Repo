1ff4b57d7b10f7b3804ea6f93a7ac09cf6c07f06
Sonu-Shettiyar <sonubadmash003@gmail.com>
2023-08-08 13:58:02 +0530

    repo-created

---
+++ S2/D2/zomato_chronicles.py
@@ -0,0 +1,38 @@
+def main():
+    print("!! Welcome To Zomato Chronicles !!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+menu = {
+    1: {
+        "name": "Pizza",
+        "price": 12.99,
+        "availability": True
+    },
+    2: {
+        "name": "Burger",
+        "price": 8.89,
+        "availability": False
+    },
+     3: {
+        "name": "Fries",
+        "price": 5.69,
+        "availability": False
+    }, 
+    4: {
+        "name": "Pasta",
+        "price": 2.99,
+        "availability": False
+    }, 
+    5: {
+        "name": "Juice",
+        "price": 6.25,
+        "availability": False
+    },
+}
+
+
+
